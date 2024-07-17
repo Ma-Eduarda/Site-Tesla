ca7d3e0a94c2c70419d1a0f7852731ce5c13d560
Maria Eduarda <eduardabritto2014@gmail.com>
2024-07-17 00:39:04 -0300

    Add files via upload

---
+++ TESLA/script.js
@@ -0,0 +1,10 @@
+let formulario = document.querySelector(".box")
+
+function aparecerForm(){
+    formulario.style.opacity = "90%"
+}
+
+function desaparecerForm(){
+    formulario.style.opacity = "-100%"
+}
+
